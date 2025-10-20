2f993e081cbf3ac613323351b292c0a0a8c9b3c4
AZil-code <avilzilberman@gmail.com>
2025-10-20 17:59:38 +0300

    Added temp ability to remove station from side bar

--- src/cmps/StationList.jsx
+++ src/cmps/StationList.jsx
@@ -12,6 +12,7 @@ export function StationList({ stations, onRemoveStation, filterTxt }) {
    }
 
    const filteredStations = stations.filter((station) => station.name.includes(filterTxt));
+   const contextMenuOptMap = new Map([['Delete', onRemoveStation]]);
 
    return (
       <div className="station-list-container">
@@ -20,9 +21,18 @@ export function StationList({ stations, onRemoveStation, filterTxt }) {
                <li
                   key={station._id}
                   onClick={() => onSelectStation(station._id)}
+                  // For future implementation
+                  // onContextMenu={(ev) => {
+                  //    ev.preventDefault();
+                  //    console.log('delet!');
+                  // }}
                   className={selectedStationId === station._id ? 'selected' : ''}
                >
                   <StationPreview station={station} />
+                  {/* Temp solution, just to have the functionality */}
+                  <button className="remove-btn" onClick={() => onRemoveStation(station._id)}>
+                     X
+                  </button>
                </li>
             ))}
          </ul>
