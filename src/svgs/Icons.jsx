1242822784204a01d5f023b7230218005a899931
AZil-code <avilzilberman@gmail.com>
2025-11-29 20:01:53 +0200

    Added dir with default css for svgs + default styling for X

--- src/svgs/Icons.jsx
+++ src/svgs/Icons.jsx
@@ -1,6 +1,6 @@
 export function X({ className }) {
    return (
-      <svg role="img" aria-label="Close" aria-hidden="false" className={className} viewBox="0 0 24 24">
+      <svg role="img" aria-label="Close" aria-hidden="false" className={`x ${className}`} viewBox="0 0 24 24">
          <path d="M3.293 3.293a1 1 0 0 1 1.414 0L12 10.586l7.293-7.293a1 1 0 1 1 1.414 1.414L13.414 12l7.293 7.293a1 1 0 0 1-1.414 1.414L12 13.414l-7.293 7.293a1 1 0 0 1-1.414-1.414L10.586 12 3.293 4.707a1 1 0 0 1 0-1.414"></path>
       </svg>
    );
