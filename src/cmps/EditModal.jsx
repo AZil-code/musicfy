c34fef87c8ef4a1ddb20b0c530ab3716ae197728
AZil-code <avilzilberman@gmail.com>
2025-11-29 22:14:37 +0200

    Improved edit modal styling

--- src/cmps/EditModal.jsx
+++ src/cmps/EditModal.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { X } from '../svgs/Icons.jsx';
 
 export function EditStationModal({ onSetIsEditOpen, station, onSaveStation }) {
    const [stationToEdit, setStationToEdit] = useState({ ...station });
@@ -19,18 +20,18 @@ export function EditStationModal({ onSetIsEditOpen, station, onSaveStation }) {
    }
 
    return (
-      <div className="modal-backdrop">
+      <div className="modal-backdrop edit-modal">
          <form className="modal-content" onSubmit={onSave}>
             <div className="modal-header">
-               <h1>Edit Details</h1>
+               <h2>Edit Details</h2>
                <button className="close-btn" onClick={onClose}>
-                  X
+                  <X />
                </button>
             </div>
             <div className="content-container">
-               <img src="https://www.afrocharts.com/images/song_cover.png" />
+               <img src={station.coverImage || 'https://www.afrocharts.com/images/song_cover.png'} />
                <input
-                  className="field-name"
+                  className="input field-name"
                   type="text"
                   name="name"
                   id="name"
@@ -38,8 +39,8 @@ export function EditStationModal({ onSetIsEditOpen, station, onSaveStation }) {
                   placeholder="Add a name"
                   value={stationToEdit.name}
                />
-               <input
-                  className="field-desc"
+               <textarea
+                  className="input field-desc"
                   type="description"
                   name="description"
                   id="description"
@@ -47,8 +48,8 @@ export function EditStationModal({ onSetIsEditOpen, station, onSaveStation }) {
                   placeholder="Add an optional description"
                   value={stationToEdit.description}
                />
+               <input className="submit-btn circle-btn" type="submit" value="Save" />
             </div>
-            <input className="submit-btn circle-btn" type="submit" value="Save" />
          </form>
       </div>
    );
